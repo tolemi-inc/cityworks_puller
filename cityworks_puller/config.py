66c30b9098188095d496460d620ebfb884ef8963
reid-nichols19 <rnichols44@gmail.com>
2024-03-26 10:10:53 -0400

    initial python commits

---
+++ cityworks_puller/config.py
@@ -0,0 +1,68 @@
+from config_error import ConfigError
+
+class Config:
+    def __init__(self, data_file_path, login_name, password, report_name, months):
+        self.data_file_path = data_file_path
+        self.login_name = login_name
+        self.password = password
+        self.report_name = report_name
+        self.months = months
+
+    @ property
+    def data_file_path(self):
+        return self._data_file_path
+
+    @ data_file_path.setter
+    def data_file_path(self, value):
+        if value is None:
+            raise ConfigError("Missing data file path in config.")
+        else:
+            self._data_file_path = value
+    
+    @ property
+    def login_name(self):
+        return self._login_name
+
+    @ login_name.setter
+    def login_name(self, value):
+        if value is None:
+            raise ConfigError("Missing login name in config.")
+        else:
+            self._login_name = value
+
+    @ property
+    def password(self):
+        return self._password
+
+    @ password.setter
+    def password(self, value):
+        if value is None:
+            raise ConfigError("Missing password in config.")
+        else:
+            self._password = value
+
+    @property
+    def report_name(self):
+        return self._report_name
+
+    @report_name.setter
+    def report_name(self, value):
+        allowed_values = ["Inspections", "Work Orders", "Cases", "Requests"]
+        if value is None:
+            raise ConfigError("Missing report name in config")
+        elif value in allowed_values:
+            self._report_name = value            
+        else:
+            raise ConfigError("Invalid report name: {}. Expecting one of {}".format(
+                value, ", ".join(allowed_values)))
+
+    @property
+    def months(self):
+        return self._months
+
+    @months.setter
+    def months(self, value):
+        if value is None:
+            raise ConfigError("Missing include number of months in config")
+        else:
+            self._months = value
---
+++ cityworks_puller/main.py
@@ -0,0 +1,98 @@
+from cityworks import Cityworks
+from config import Config
+from config_error import ConfigError
+import json
+import traceback
+import argparse
+import logging
+
+parser = argparse.ArgumentParser(
+    description='Process inputs')
+
+parser.add_argument('--config', type=str, help='Path to config file')
+
+args = parser.parse_args()
+
+logging.getLogger().setLevel(logging.INFO)
+
+def run(config):
+
+    cityworks = Cityworks(config.login_name, config.password, "https://cityworks.toledo.oh.gov/Cityworks/services")
+
+    token = cityworks.get_access_token()
+
+    report_name = config.report_name
+    try:
+        months_to_include = int(config.months)
+    except:
+        raise Exception("Unable to convert number of months to integer")
+
+    if report_name == 'Cases':
+        case_object_ids = cityworks.search_cases(token, months_to_include)
+        out_data = cityworks.get_cases_by_ids(token, case_object_ids)
+    elif report_name == 'Inspections':
+        inspection_ids = cityworks.search_inspections(token, months_to_include)
+        out_data = cityworks.get_inspections_by_ids(token, inspection_ids)
+    elif report_name == 'Work Orders':
+        work_order_ids = cityworks.search_work_orders(token, months_to_include)
+        out_data = cityworks.get_work_orders_by_ids(token, work_order_ids)
+    elif report_name == 'Requests':
+        request_ids = cityworks.search_requests(token, months_to_include)
+        out_data = cityworks.get_requests_by_ids(token, request_ids)
+    
+    csv_headers = cityworks.create_csv(out_data, config.data_file_path)
+    headers_dict = [{"name": header, "type": "VARCHAR"} for header in csv_headers]
+
+    output_object = {'status': 'ok',
+                     'file_name': config.data_file_path, 'columns': headers_dict}
+    print('DONE', json.dumps(output_object))
+
+
+def fail(error):
+    result = {
+        "status": "error",
+        "error": """{}
+         {}""".format(str(error), traceback.format_exc())
+    }
+
+    output_json = json.dumps(result)
+    print('DONE', output_json)
+
+
+def load_config(file_path):
+    raw_config = load_json(file_path)
+    # print('RAW CONFIG', raw_config)
+
+    data_file_path = raw_config.get('dataFilePath', None)
+
+    sub_config = raw_config.get('config')
+    login_name = sub_config.get('login_name')
+    password = sub_config.get('password')
+    report_name = sub_config.get('report_name')
+    months = sub_config.get('months')
+
+    return Config(data_file_path, login_name, password, report_name, months)
+
+
+def load_json(file_path):
+    try:
+        with open(file_path, 'r') as file:
+            data = json.load(file)
+        return data
+    except FileNotFoundError:
+        True
+        print(f"File '{file_path}' not found.")
+    except json.JSONDecodeError as e:
+        True
+        print(f"JSON decoding error: {e}")
+    except Exception as e:
+        True
+        print(f"An error occurred: {e}")
+
+# Main Program
+if __name__ == "__main__":
+    try:
+        config = load_config(args.config)
+        run(config)
+    except ConfigError as e:
+        fail(e)
