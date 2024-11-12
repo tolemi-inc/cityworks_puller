1918eb037325af0adc300bb62568a6a2acc73bfc
reid-nichols19 <rnichols44@gmail.com>
2024-11-12 12:37:59 -0500

    add methods for getting case payments, tasks, corrections, add filter for getting case by type

--- cityworks_puller/cityworks.py
+++ cityworks_puller/cityworks.py
@@ -57,7 +57,7 @@ class Cityworks:
 
         return response["Value"]["Token"]
     
-    def search_objects(self, token, url, months=None, filter_criteria=None):
+    def search_objects(self, token, url, filter_criteria=None):
         if filter_criteria:
             payload = {
                 "token": token,
@@ -83,9 +83,9 @@ class Cityworks:
         start = end - relativedelta(months=months)
         return {start_date_text: start.strftime("%Y-%m-%d"), end_date_text: end.strftime("%Y-%m-%d")}
     
-    def search_cases(self, token):
+    def search_cases(self, token, filter_criteria=None):
         url = f"{self.base_url}/Pll/CaseObject/Search"
-        cases = self.search_objects(token, url)
+        cases = self.search_objects(token, url, filter_criteria)
         return cases 
 
     def search_inspections(self, token, months=1):
@@ -110,6 +110,13 @@ class Cityworks:
         url = f"{self.base_url}/Pll/CaseAddress/SearchObject"
         requests = self.search_objects(token, url)
         return requests 
+    
+    def search_case_tasks(self, token):
+        url = f"{self.base_url}/Pll/CaseTask/Search"
+        filter_criteria = {'TaskComplete': False}
+        requests = self.search_objects(token, url, filter_criteria)
+        print(len(requests))
+        return requests 
 
     def get_object_by_ids(self, token, url, ids, id_name, batch_size=500):
         objects = []
@@ -132,8 +139,14 @@ class Cityworks:
         return cases
     
     def get_recent_case_ids(self, token, months):
-        case_object_ids = self.search_cases(token)
-        cases = self.get_cases_by_ids(token, case_object_ids)
+        code_enforcement_case_types = ['CE-CASE', 'CE-COURT']
+        all_case_object_ids = []
+        for case_type in code_enforcement_case_types:
+            filter_criteria = {'CaseType': case_type}
+            case_object_ids = self.search_cases(token, filter_criteria)
+            all_case_object_ids.extend(case_object_ids)
+
+        cases = self.get_cases_by_ids(token, all_case_object_ids)
 
         cutoff = pd.Timestamp(date.today() - relativedelta(months=months))
         cases['DateModified'] = pd.to_datetime(cases['DateModified'], format='%Y-%m-%dT%H:%M:%SZ', errors='coerce')
@@ -143,6 +156,7 @@ class Cityworks:
             (cases['DateModified'] >= cutoff) | 
             (cases['DateModified'].isna() & (cases['DateEntered'] >= cutoff))
         ]
+
         return recent_cases['CaObjectId'].tolist()
 
     def get_inspections_by_ids(self, token, ids):
@@ -184,6 +198,26 @@ class Cityworks:
             i += 1
         logging.info(f"Successfully got case fees from Cityworks")
         return pd.DataFrame(fees)
+    
+    def get_case_payments_by_id(self, token, case_ids):
+        url = f"{self.base_url}/Pll/CasePayment/ByCaObjectId"
+        i = 1
+        num_cases = len(case_ids)
+        payments = []
+        for case_id in case_ids:
+            payload = {
+                "token": token,
+                "data": json.dumps({'CaObjectId': case_id})   
+            }
+            payment_response = self.make_api_call("GET", url, payload)
+            if payment_response["Value"] == None or len(payment_response["Value"]) == 0:
+                logging.info(f"Case {i} out of {num_cases} has no payments")
+            else:
+                logging.info(f"Case {i} out of {num_cases} has payments")
+                payments.extend(payment_response["Value"])
+            i += 1
+        logging.info(f"Successfully got case payments from Cityworks")
+        return pd.DataFrame(payments)
 
     def get_case_tasks_by_id(self, token, case_ids):
         url = f"{self.base_url}/Pll/CaseTask/ByCaObjectId"
@@ -202,6 +236,12 @@ class Cityworks:
             i += 1
         logging.info(f"Successfully got case tasks from Cityworks")
         return pd.DataFrame(tasks)
+    
+    def get_task_corrections_by_id(self, token, task_ids):
+        url = f"{self.base_url}/Pll/CaseCorrections/ByCaTaskIds"
+        task_ids = pd.read_csv('case_task_ids.csv')
+        corrections = self.get_object_by_ids(token, url, task_ids['cataskid'].tolist(), "CaTaskIds")
+        return corrections
 
     def get_cases_with_addresses(self, token):
         case_addresses = pd.DataFrame(self.search_case_addresses(token))
@@ -217,7 +257,7 @@ class Cityworks:
         try:
             if len(data) > 0:
                 field_names = data.columns.tolist()
-                data.to_csv(path, header=False, index=False)
+                data.to_csv(path, header=True, index=False)
                 return field_names
             else:
                 return []
